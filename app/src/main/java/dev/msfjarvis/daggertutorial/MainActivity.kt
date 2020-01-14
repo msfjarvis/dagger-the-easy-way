9c0b2ed6fd459e53a00ab1b69bd979020f361e3b
Harsh Shandilya <msfjarvis@gmail.com>
2020-01-14 19:58:00 +0530

    Inject SharedPreferences into MainActivity through Dagger
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/src/main/java/dev/msfjarvis/daggertutorial/MainActivity.kt
+++ app/src/main/java/dev/msfjarvis/daggertutorial/MainActivity.kt
@@ -1,17 +1,16 @@
 package dev.msfjarvis.daggertutorial
 
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.preference.PreferenceManager
 
 class MainActivity : AppCompatActivity() {
+
+    private val prefs = ExampleApplication.component.preferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val prefs: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         if (prefs.getBoolean("first_start", true)) {
             Toast.makeText(this, "First start!", Toast.LENGTH_LONG).show()
             prefs.edit().putBoolean("first_start", false).apply()
