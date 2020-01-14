f1a60ffaf6f07f8654bde27fbd65bef08c248f4e
Harsh Shandilya <msfjarvis@gmail.com>
2020-01-14 19:36:18 +0530

    Introduce SharedPreferences into object graph
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- app/src/main/java/dev/msfjarvis/daggertutorial/di/AppComponent.kt
+++ app/src/main/java/dev/msfjarvis/daggertutorial/di/AppComponent.kt
@@ -1,16 +1,25 @@
 package dev.msfjarvis.daggertutorial.di
 
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.preference.PreferenceManager
 import dagger.Component
 import dagger.Module
+import dagger.Provides
+import dagger.Reusable
 import javax.inject.Singleton
 
 @Singleton
 @Component(modules = [AppModule::class])
 interface AppComponent {
 
+    val preferences: SharedPreferences
 }
 
 @Module
 object AppModule {
 
+    @Provides
+    @Reusable
+    fun provideSharedPrefs(context: Context): SharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 }
