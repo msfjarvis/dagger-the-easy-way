a086c951bea4829a0987b2d14a3c75cfb93b650f
Harsh Shandilya <me@msfjarvis.dev>
2020-03-06 01:33:36 +0530

    MainActivity: Create and use CounterPresenter
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/dev/msfjarvis/daggertutorial/MainActivity.kt
+++ app/src/main/java/dev/msfjarvis/daggertutorial/MainActivity.kt
@@ -1,12 +1,34 @@
 package dev.msfjarvis.daggertutorial
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import dev.msfjarvis.daggertutorial.di.AppModule
+import dev.msfjarvis.daggertutorial.di.CounterPresenter
+import dev.msfjarvis.daggertutorial.di.CounterScreenModule
+import dev.msfjarvis.daggertutorial.di.DaggerAppComponent
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var presenter: CounterPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val appComponent = DaggerAppComponent.builder()
+            .appModule(AppModule())
+            .build()
+
+        val counterScreenComponent = appComponent
+            .counterScreenComponent(CounterScreenModule())
+        counterScreenComponent.inject(this)
+        Log.d(TAG, presenter.counter.name)
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
