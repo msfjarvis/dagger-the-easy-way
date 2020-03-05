7cb0db6b7791668ff1dbe1be7ba3a333d8d21a93
Harsh Shandilya <me@msfjarvis.dev>
2020-03-06 01:30:03 +0530

    Add Counter and CounterPresenter
    Signed-off-by: Harsh Shandilya <me@msfjarvis.dev>

--- app/src/main/java/dev/msfjarvis/daggertutorial/di/AppComponent.kt
+++ app/src/main/java/dev/msfjarvis/daggertutorial/di/AppComponent.kt
@@ -4,6 +4,9 @@ import dagger.Component
 import dagger.Module
 import javax.inject.Singleton
 
+data class Counter(val name: String)
+class CounterPresenter(val counter: Counter)
+
 @Singleton
 @Component(modules = [AppModule::class])
 interface AppComponent
