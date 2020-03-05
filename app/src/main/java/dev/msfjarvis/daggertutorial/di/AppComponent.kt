b2004bb4d75b65a54e741d6c71410654922ee4f6
Harsh Shandilya <me@msfjarvis.dev>
2020-03-06 01:28:09 +0530

    Initial commit
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

---
+++ app/src/main/java/dev/msfjarvis/daggertutorial/di/AppComponent.kt
@@ -0,0 +1,12 @@
+package dev.msfjarvis.daggertutorial.di
+
+import dagger.Component
+import dagger.Module
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [AppModule::class])
+interface AppComponent
+
+@Module
+class AppModule
